c898c7721856c15d41581582a3289472a473b2e7
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-15 18:47:51 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -67,7 +67,7 @@ if (message.content.startsWith(`${prefix}serverinfo`)) {
   }
 	
 	if (message.content.startsWith(`${prefix}urban`)) {
-		let args = message.content.slice(1).split(" "); 
+let args = message.content.split(/ +/g).slice(1)
 	if (args.length < 1) return message.channel.send("Enter in a word you want the definition for.")
 let str = args.join(" ");
 
