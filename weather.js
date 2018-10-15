12cd89a820b5d80add3d93e77b04816af28413cd
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-15 18:35:55 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -77,7 +77,7 @@ urban(str).first(json => {
 
 let defineembed = new Discord.RichEmbed()
     .setTitle(json.word)
-    .setDescription(json.definition || "None")
+    .setDescription(json.definition)
     .setColor(0x374f6b)
     .addField("Upvotes", json.thumbs_up, true)
     .addField("Downvotes", json.thumbs_down, true)
