941b8a63c69326f7d7236832445c41c6895968e7
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2018-10-16 03:37:27 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -78,7 +78,7 @@ urban(str).first(json => {
  if(!json) return message.channel.send("No results found from this search.")
 
 let defineembed = new Discord.RichEmbed()
-    .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+    .setAuthor("Urban Dictionary", "https://imgur.com/a/BermgL3.png")
     .setTitle(json.word)
     .setDescription(json.definition)
     .setColor(0x374f6b)
