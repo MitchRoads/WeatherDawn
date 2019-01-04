6741e91ee680c0ef22eed9519bed9789c04d5aa0
BotManRoads <42738913+MitchRoads@users.noreply.github.com>
2019-01-03 21:31:50 -0500

    Update weather.js

--- weather.js
+++ weather.js
@@ -92,7 +92,7 @@ if(!user) return message.channel.send("You haven't selected/mentioned a user who
             .addField('User ID', user.id, true)
             .addField('Current Tag', user.tag, true)
             .addField('Server Nickname', `${player.displayName}`, true) 
-            .addField('Highest Member Role', `${player.highestRole.name}`, true)
+            .addField('Highest Member Role', `<@&${player.highestRole.id}>`, true)
             .addField(`Roles [${rolesize}]`, `${roles}`)
             .addField('Game/Playing', `${(user.presence.game && user.presence.game && user.presence.game.name) || 'None'}`, true)
             .addField('Status', user.presence.status, true)
